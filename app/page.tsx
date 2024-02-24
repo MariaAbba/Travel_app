f69e1dc3f1293d9ef99c73f95cf0a8c760d4c52b
Mary <gretchen7887@gmail.com>
2024-02-24 22:37:46 +0400

    Upd

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
-import Image from 'next/image'
-
 export default function Home() {
-  return <h1 className="text-bold text-teal-500">Hello World</h1>
+  return <h1 className="font-bold text-teal-500 text-2xl">
+    
+  </h1>
 }
+
