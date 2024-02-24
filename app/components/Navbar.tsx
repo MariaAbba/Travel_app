ea247c3cede7d2fa72f5b9bc98428e95cc555d66
Mary <gretchen7887@gmail.com>
2024-02-24 23:08:10 +0400

    Upd

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,10 +1,11 @@
-import Link from "next/link"
+import Image from 'next/image'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <nav className="border-2 border-red-500 flexBetween max-container padding-container relative z-30 py-5">
-      <Link href='/'>
-        
+    <nav className="flexBetween max-container padding-container relative z-30 py-5">
+      <Link href="/">
+        <Image src="/hilink-logo.svg" alt="logo" height={29} width={74} />
       </Link>
     </nav>
   )
