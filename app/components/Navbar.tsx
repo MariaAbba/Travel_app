a673a7444c5bf05413b94e385dd213304859252f
Mary <gretchen7887@gmail.com>
2024-02-24 22:54:07 +0400

    Upd

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,5 +1,13 @@
+import Link from "next/link"
+
 const Navbar = () => {
-  return <nav className="border-2 border-red-500 flexBetween max-container padding-container relative z-30 py-5"></nav>
+  return (
+    <nav className="border-2 border-red-500 flexBetween max-container padding-container relative z-30 py-5">
+      <Link href='/'>
+        
+      </Link>
+    </nav>
+  )
 }
 
 export default Navbar
