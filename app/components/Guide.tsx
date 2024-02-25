4ac70359b8e7b5daf8b2690112a2c2a723d55e21
Mary <gretchen7887@gmail.com>
2024-02-25 23:01:47 +0400

    Upd

--- app/components/Guide.tsx
+++ app/components/Guide.tsx
@@ -4,12 +4,12 @@ import React from 'react'
 const Guide = () => {
   return (
     <section className="flexCenter flex-col">
-      <div className="padding-container max-cotainer w-full pb-24">
+      <div className="padding-container max-container w-full pb-24">
         <Image src="/camp.svg" alt="camp" width={50} height={50} />
         <p className="uppercase regular-18 -mt-1 mb-3 text-green-50">
           We are here for you
         </p>
-        <div className="flex flex-wrap justify-between gap-5 lg:gap-5">
+        <div className="flex flex-wrap justify-between gap-5 lg:gap-10">
           <h2 className="bold-40 lg:bold-64 xl:max-w-[390px]">
             Guide you to easy path
           </h2>
