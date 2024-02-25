767a72e25d8ae61fdc8d561a9f5fac0fa36dcb96
Mary <gretchen7887@gmail.com>
2024-02-25 14:18:26 +0400

    Upd

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { NAV_LINKS } from '../constants/constants'
 
 const Navbar = () => {
   return (
@@ -7,6 +8,17 @@ const Navbar = () => {
       <Link href="/">
         <Image src="/hilink-logo.svg" alt="logo" height={29} width={74} />
       </Link>
+      <ul className="hidden h-full gap-12 lg:flex">
+        {NAV_LINKS.map((link) => (
+          <Link
+            href={link.href}
+            key={link.key}
+            className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
     </nav>
   )
 }
