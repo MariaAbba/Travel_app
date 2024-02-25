9f4427b97d23e5d5e160c9190efb571d9a859d42
Mary <gretchen7887@gmail.com>
2024-02-25 15:13:15 +0400

    Upd

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,9 +1,23 @@
+import Image from 'next/image'
 import React from 'react'
 
-const Button = () => {
-  return <button type="button">
-    
-  </button>
+type ButtonProps = {
+  type: 'button' | 'submit'
+  title: string
+  icon?: string
+  variant: 'btn_dark_green'
+}
+
+const Button = ({ type, title, icon, variant }) => {
+  return (
+    <button
+      className={`flexCenter gap-3 rounded-full border ${variant}`}
+      type={type}
+    >
+      {icon && <Image src={icon} alt={title} width={24} height={24} />}
+      <label className="bold-16 whitespace-nowrap">{title}</label>
+    </button>
+  )
 }
 
 export default Button
