c20bb957b8e5f6faf6218253574b07ce12ddcf88
Mary <gretchen7887@gmail.com>
2024-02-25 22:05:37 +0400

    Upd

--- app/components/Features.tsx
+++ app/components/Features.tsx
@@ -1,9 +1,70 @@
+import Image from 'next/image'
 import React from 'react'
+import { FEATURES } from '../constants/constants'
 
 const Features = () => {
   return (
-    <div>Features</div>
+    <section className="flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24">
+      <div className=" flex max-container padding-container relative w-full justify-end">
+        <div className="flex flex-1 lg:min-h-[900px]">
+          <Image
+            src="/phone.png"
+            alt="phone"
+            width={440}
+            height={1000}
+            className="feature-phone"
+          />
+        </div>
+
+        <div className="z-20 flex w-full flex-col lg:w-[60%]">
+          <div className="relative">
+            <Image
+              src="/camp.svg"
+              alt="camp"
+              width={50}
+              height={50}
+              className="absolute left-[-5px] top-[-28px] lg:w-[50px] w-10"
+            />
+            <h2 className="bold-40 lg:bold-64">Our features</h2>
+          </div>
+          <ul className="mt-10 grid gap-10 md:grid-cols-2 lg:mg-20 lg:gap-20">
+            {FEATURES.map((feature) => (
+              <FeatureItem
+                key={feature.title}
+                title={feature.title}
+                icon={feature.icon}
+                description={feature.description}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    </section>
   )
 }
 
+type FeatureItem = {
+  title: string
+  icon: string
+  description: string
+}
+
+const FeatureItem = ({ title, icon, description }: FeatureItem) => {
+  return (
+    <li className="flex w-full flex-1 flex-col items-start">
+      <div className="rounded-full p-4 lg:p-7 bg-green-50">
+        <Image src={icon} alt="map" width={28} height={28} />
+      </div>
+      <h2 className="bold-20 lg:bold-32 mt-5 capitalize">{title}</h2>
+      <p className='regular-16 mt-5 bg-white'>{description}</p>
+    </li>
+  )
+}
 export default Features
+
+//  <Image
+//     src="/camp.svg"
+//     alt="camp"
+//     width={50}
+//     height={50}
+//      />
--- app/components/Guide.tsx
+++ app/components/Guide.tsx
@@ -1,8 +1,67 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Guide = () => {
   return (
-    <div>Guide</div>
+    <section className="flexCenter flex-col">
+      <div className="padding-container max-cotainer w-full pb-24">
+        <Image src="/camp.svg" alt="camp" width={50} height={50} />
+        <p className="uppercase regular-18 -mt-1 mb-3 text-green-50">
+          We are here for you
+        </p>
+        <div className="flex flex-wrap justify-between gap-5 lg:gap-5">
+          <h2 className="bold-40 lg:bold-64 xl:max-w-[390px]">
+            Guide you to easy path
+          </h2>
+          <p className="regular-16 text-gray-30 xl:max-w-[520px]">
+            Only with the hilink application you will no longer get lost and get
+            lost again, because we already support offline maps when there is no
+            Internet connection in the field. Invite your className relatives
+            and friends to have fun in the wilderness through the valley and
+            reach the top of the mountain.{' '}
+          </p>
+        </div>
+      </div>
+
+      <div className="flexCenter max-container relative w-full">
+        <Image
+          src="/boat.png"
+          alt="boat"
+          width={1440}
+          height={580}
+          className="w-full object-cover object-center 2xl:rounded-5xl"
+        />
+
+        <div className="absolute flex bg-white py-8 pl-5 pr-7 gap-3 rounded-3xl border shadow-md md:left-[5%] lg:top-20 ">
+          <Image
+            src="/meter.svg"
+            alt="meter"
+            width={16}
+            height={15}
+            className="h-full w-auto"
+          />
+
+          <div className="flexBetween flex-col">
+            <div className="flex w-full flex-col">
+              <div className="flexBetween w-full">
+                <p className="regular-16 text-gray-20">Destination</p>
+                <p className="bold-16 text-green-50">48 min</p>
+              </div>
+              <p className="bold-20 mt-2">Aguas Calientes</p>
+            </div>
+
+            <div className="flex w-full flex-col">
+              <p className="regular-16 text-gray-20">Start track</p>
+              <p className="bold-16 text-green-50">48 min</p>
+
+              <h4 className="bold-20 mt-2 whitespace-nowrap">
+                Wonorejo Pasuruan
+              </h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
