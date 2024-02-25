5244ddfbc3018d29de8b898437e70af4849529cf
Mary <gretchen7887@gmail.com>
2024-02-25 18:53:48 +0400

    Upd

--- app/components/Camp.tsx
+++ app/components/Camp.tsx
@@ -1,8 +1,61 @@
+import Image from 'next/image'
 import React from 'react'
+import { PEOPLE_URL } from '../constants/constants'
+
+interface CampProps {
+  backgroundImage: string
+  title: string
+  subtitle: string
+  peopleJoined: string
+}
+
+const CampSite = ({ backgroundImage, title, subtitle, peopleJoined }) => {
+  return (
+    <div
+      className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}
+    >
+      <div className="flex flex-col h-full items-start justify-between p-6 lg:px-20 lg:py-10 ">
+        <div className="flexCenter gap-4">
+          <div className="rounded-full bg-green-50 p-4">
+            <Image src="/folded-map.svg" alt="map" width={28} height={28} />
+          </div>
+
+          <div className="flex flex-col gap-1">
+            <h4 className="bold-18 text-white">{title}</h4>
+            <p className="regular-14 text-white">{subtitle}</p>
+          </div>
+        </div>
+
+        <div className="flexCenter gap-6 ">
+          <span className="flex -space-x-4 overflow-hidden">
+            {PEOPLE_URL.map((url) => (
+              <Image 
+              src="url" 
+              key={url}
+               alt="person" 
+               width={52} 
+               height={52} />
+            ))}
+          </span>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 const Camp = () => {
   return (
-    <div>Camp</div>
+    <section className="border-2 border-green-500 2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20">
+      <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
+        <CampSite
+          backgroundImage="bg-bg-img-1"
+          title="Putuk Truno Camp"
+          subtitle="Prigen, Pasuran"
+          peopleJoined="50+ Joined"
+        />
+        <CampSite />
+      </div>
+    </section>
   )
 }
 
--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -4,7 +4,7 @@ import Button from './Button'
 
 const Hero = () => {
   return (
-    <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row border-2 border-red-500">
+    <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
       <div className="hero-map" />
       <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
         <Image
@@ -50,6 +50,32 @@ const Hero = () => {
             icon="/play.svg"
             variant="btn_white_text"
           />
+        </div>
+      </div>
+
+      <div className="relative flex flex-1 items-start ">
+        <div className="relative z-20 w-[268px] flex flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8">
+          <div className="flex flex-col">
+            <div className="flexBetween">
+              <p className="regular-16 text-gray-20">Location</p>
+              <Image src="/close.svg" alt="close" width={24} height={24} />
+            </div>
+            <p className="bold-20 text-white">Aguas Calientes</p>
+          </div>
+
+          <div className="flexBetween">
+            <div className="flex flex-col">
+              <p className="regular-16 block text-gray-20">Distance</p>
+              <p className="bold-20 text-white">173.28 mi</p>
+            </div>
+
+            <div className="flex flex-col">
+              <p className="regular-16 block text-gray-20">Elevation</p>
+              <p className="bold-20 text-white">2.040 km</p>
+            </div>
+
+
+          </div>
         </div>
       </div>
     </section>
