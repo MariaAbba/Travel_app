3b97acf8334f52b1ebf5c9a524bd421b97e263e7
Mary <gretchen7887@gmail.com>
2024-02-26 15:30:05 +0400

    Fixed the name of the website

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 
 export const metadata: Metadata = {
-  title: 'HiLink',
+  title: 'Hilink',
   description: 'Travel UI/UX App for Camping',
 }
 
